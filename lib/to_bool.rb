5c9ed38e47c638725e33530ea1a8aec96281af20
Jon Frisby <jfrisby@mrjoy.com>
2012-08-22 22:16:03 -0700

    Touching up doc comments.

--- lib/to_bool.rb
+++ lib/to_bool.rb
@@ -4,18 +4,19 @@
 #
 class Object
   ##
-  # Monkey-patch for Object to turn certain values into TrueClass or FalseClass
+  # Monkey-patch for `Object` to turn certain values into `true` or `false`
   # based on what you'd intuitively expect when dealing with HTTP request
   # parameters.  Specifically, things that coerce to strings like "true", "t",
-  # or "1" becomes true, things that coerce to strings like "false", "", "no",
-  # or "0" become false -- and anything that's not pretty unambiguous causes
-  # an exception.
+  # or "1" becomes `true`, things that coerce to strings like "false", "",
+  # "no", or "0" become `false` -- and anything that's not fairly unambiguous
+  # causes an exception.
   #
   # Specifically:
-  #   Returns true, if #to_s =~ /^(true|t|yes|y|1)$/i
-  #   Returns false, if #to_s =~ /^(false|f|no|n|0|)$/i
-  #     Note that the empty string is considered false.
-  #   Otherwises, raises ArgumentError.
+  #
+  # * Returns `true`, if `#to_s =~ /^(true|t|yes|y|1)$/i`
+  # * Returns `false`, if `#to_s =~ /^(false|f|no|n|0|)$/i`
+  #   * Note that the empty string is considered `false`.
+  # * Otherwises, raises `ArgumentError`.
   #
   def to_bool
     return true if (self.to_s =~ /^(true|t|yes|y|1)$/i)
@@ -30,8 +31,8 @@ end
 #
 class Fixnum
   ##
-  # Monkey-patch for Fixnum to turn non-zero values into true, and zero into
-  # false.  Matches the API for the Object#to_bool monkey-patch.
+  # Monkey-patch for `Fixnum` to turn non-zero values into `true`, and zero into
+  # `false`.  Matches the API for the `Object#to_bool` monkey-patch.
   #
   def to_bool
     return (self != 0)
@@ -44,8 +45,8 @@ end
 #
 class NilClass
   ##
-  # Monkey-patch for NilClass to match the API for the Object#to_bool monkey
-  # patch.  Returns false.
+  # Monkey-patch for `NilClass` to match the API for the `Object#to_bool`
+  # monkey patch.  Returns `false`.
   #
   def to_bool
     return false
@@ -58,8 +59,8 @@ end
 #
 class TrueClass
   ##
-  # Monkey-patch for TrueClass to match the API for the Object#to_bool monkey
-  # patch.  Returns true.
+  # Monkey-patch for `TrueClass` to match the API for the `Object#to_bool`
+  # monkey patch.  Returns `true`.
   #
   def to_bool
     return self
@@ -72,8 +73,8 @@ end
 #
 class FalseClass
   ##
-  # Monkey-patch for FalseClass to match the API for the Object#to_bool monkey
-  # patch.  Returns false.
+  # Monkey-patch for `FalseClass` to match the API for the `Object#to_bool`
+  # monkey patch.  Returns `false`.
   #
   def to_bool
     return self
